5dcbd537a7fca317530741d6b90b52f84bd2e02c
Dennis Skoko <dennis.skoko@hotmail.com>
2018-08-25 21:13:17 +0200

    Adds material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 
-class App extends Component {
-  render () {
-    return (
-      <h1>Goodies</h1>
-    )
+const App = ({ classes }) => (
+  <CssBaseline>
+    <Typography className={classes.title} variant='display4'>
+      Goodies
+    </Typography>
+  </CssBaseline>
+)
+
+const styles = {
+  title: {
+    textAlign: 'center'
   }
 }
 
-export default App
+export default withStyles(styles)(App)
